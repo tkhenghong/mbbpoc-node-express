b63ddd4f195c375b8871521b0615b3e87dc4e447
Teoh Kheng Hong <tkhenghong@gmail.com>
2022-07-05 00:35:07 +0800

    Fixed issue of environment variables

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if (!isProduction) {
     mongoose.set('debug', true);
 }
 
-console.log('app.js MONGO_URI: ', MONGO_URI);
+console.log('app.js MONGO_URI: ', MONGODB_URI);
 mongoose.connect(MONGODB_URI);
 
 
